3fbd1ca3c4b4b62a9d72a107147cef7a3a8cec23
Abraham Linkon <37959190+linkon63@users.noreply.github.com>
2023-04-05 00:09:33 +0600

    dD

---
+++ src/Components/handle-form/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import Home from "./components/Home.vue";
+import About from "./components/About.vue";
+import Contact from "./components/Contact.vue";
+
+export default {
+  data() {
+    return {
+      componentToRender: Home,
+    };
+  },
+  components: {
+    Home,
+    About,
+    Contact,
+  },
+};
+</script>
+<template>
+  <div class="container mt-5">
+    <button @click="componentToRender = 'Home'">Home</button>
+    <button @click="componentToRender = 'Contact'">Contact</button>
+    <button @click="componentToRender = 'About'">About</button>
+    <keep-alive>
+      <component :is="componentToRender"></component>
+    </keep-alive>
+  </div>
+</template>
---
+++ src/Components/handle-form/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+// import './style.css'
+// main js updated
+
+createApp(App).mount('#app')
